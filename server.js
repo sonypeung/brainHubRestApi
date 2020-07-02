367013cb0707236c0072f22cbf286ead63c52db5
Sony Peung <speung@sii.pl>
2020-07-02 20:27:58 +0200

    added cors package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 
@@ -8,7 +9,7 @@ const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to the database'));
-
+app.use(cors());
 app.use(express.json());
 
 const eventRouter = require('./routes/event');
